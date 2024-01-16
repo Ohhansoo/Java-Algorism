e8e82876a7ef2e2ee1a49e400df885dfcc2b3662
hansoo <zhdnrk124@naver.com>
2024-01-16 22:09:14 +0900

    feat: init commit - SpringBoot 프로젝트 빌드

---
+++ src/main/java/com/algorism/AlgorismApplication.java
@@ -0,0 +1,15 @@
+package com.algorism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorismApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AlgorismApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/com/algorism/AlgorismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
