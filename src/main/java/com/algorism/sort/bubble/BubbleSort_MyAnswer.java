71fdd343337668075524a73f5ce74faa23c15bc3
hansoo <zhdnrk124@naver.com>
2024-01-22 22:51:52 +0900

    [6일차] 버블정렬 끝

---
+++ src/main/java/com/algorism/sort/bubble/BubbleSort_MyAnswer.java
@@ -0,0 +1,47 @@
+package com.algorism.sort.bubble;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BubbleSort_MyAnswer {
+
+    //[Tip] N의 최대값이 1,000으로 웬만한 방법을 적용하여도 풀이가 가능하다.
+    //O(n2)인 버블정력을 사용해도 시간복잡도 안에 문제를 해결할 수 있다.
+    public void sortingNumbers01() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //1. 개수를 변수에 담는다.
+        int cntNum = Integer.parseInt(st.nextToken());
+
+        //2. 수 정렬을 int[]에 담는다.
+        int[] arrNum = new int[cntNum];
+
+        for(int i = 0; i < cntNum; i++){
+            st = new StringTokenizer(br.readLine());
+            arrNum[i] =  Integer.parseInt(st.nextToken());
+        }
+
+        //3-1. 순서대로 i와 i+1을 비교해 i+1이 더 작으면 서로 위치를 바꾼다.
+        //3-2. length까지 마치면 마지막 index를 제외하고 3-1을 돌린다.
+        for(int i = 0; i < cntNum - 1; i++){
+            for(int j = 0; j < cntNum - 1 - i; j++){
+                if(arrNum[j] > arrNum[j+1]){
+                    int swapNum = arrNum[j];
+                    arrNum[j] = arrNum[j+1];
+                    arrNum[j+1] = swapNum;
+                }
+            }
+        }
+
+        for(int i = 0; i < cntNum; i++){
+            System.out.println(arrNum[i]);
+        }
+    }
+
+}
