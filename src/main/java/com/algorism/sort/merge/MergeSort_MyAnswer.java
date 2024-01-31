f6c1c776085138450d9d3151c946d7ea566c5454
hansoo <zhdnrk124@naver.com>
2024-01-31 22:38:52 +0900

    [12일차] 병합정렬 - 해설2

--- src/main/java/com/algorism/sort/merge/MergeSort_MyAnswer.java
+++ src/main/java/com/algorism/sort/merge/MergeSort_MyAnswer.java
@@ -1,16 +1,18 @@
 package com.algorism.sort.merge;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class MergeSort_MyAnswer {
+
+
+    public static int[] A, temp;
+    public static long result;
 
     /*
      * [TIP] N의 최대 범위가 1,000,000 이므로 O(nlogn)의 시간 복잡도로 정렬을 수행하면 된다.
      *      앞에서 배운 병합 정렬로 정렬을 수행한 후 결과 출력
      * */
-    public static int[] A, temp;
-    public static long result;
-
     public void sortNumber() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -71,4 +73,64 @@ public class MergeSort_MyAnswer {
         }
     }
 
+    /*
+     * [TIP] N의 최대 범위가 1,000,000 이므로 O(nlogn)의 시간 복잡도로 정렬을 수행하면 된다.
+     *      앞에서 배운 병합 정렬로 정렬을 수행한 후 결과 출력
+     * */
+    public void sortProgram02() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        A = new int[N+1];
+        temp = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        result = 0;
+        merge_sort2(1, N);
+        System.out.println(result);
+    }
+
+    private static void merge_sort2(int s, int e) {
+        if (e - s < 1) return;
+        int m = s + (e - s) / 2;
+
+        //재귀 함수 형태로 구현하기
+        merge_sort2(s, m);
+        merge_sort2(m + 1, e);
+
+        for (int i = s; i <= e; i++) {
+            temp[i] = A[i];
+        }
+        int k = s;
+        int index1 = s;
+        int index2 = m + 1;
+
+        //두 그룹을 병합하는 로직
+        while (index1 <= m && index2 <= e) {
+            if (temp[index1] > temp[index2]) {
+                A[k] = temp[index2];
+                //뒤쪽 데이터 값이 작은 경우 result 업데이트
+                result = result + index2 - k;
+                k++;
+                index2++;
+            }
+            else {
+                A[k] = temp[index1];
+                k++;
+                index1++;
+            }
+        }
+        while (index1 <= m) {
+            A[k] = temp[index1];
+            k++;
+            index1++;
+        }
+        while (index2 <= e) {
+            A[k] = temp[index2];
+            k++;
+            index2++;
+        }
+    }
 }
