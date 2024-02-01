3d5db601475cd23424d36cb9f4de441a408cbe4e
hansoo <zhdnrk124@naver.com>
2024-02-01 23:54:38 +0900

    [13일차] 기수정렬 - 개요

---
+++ src/main/java/com/algorism/sort/radix/RadixSort_MyAnswer.java
@@ -0,0 +1,20 @@
+package com.algorism.sort.radix;
+
+import java.io.IOException;
+
+/**
+ * 기수 정렬 : 값을 비교하지 않는 특이한 정렬
+ *           값을 놓고 비교할 자릿수를 정한 다음 해당 자릿수만 비교한다.
+ *           시간 복잡도는 O(kn)으로, 여기서 k는 데이터의 자릿수를 말한다.
+ *           시간 복잡도가 가장 짧은 정렬로 데이터 개수가 많을 경우 고려해본다.
+ * */
+public class RadixSort_MyAnswer {
+
+    /*
+     * [TIP] N의 최대 범위가 1,000,000 이므로 O(nlogn)의 시간 복잡도로 정렬을 수행하면 된다.
+     *      앞에서 배운 병합 정렬로 정렬을 수행한 후 결과 출력
+     * */
+    public void sortingNumbers03() throws IOException {
+
+    }
+}
