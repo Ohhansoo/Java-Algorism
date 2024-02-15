c0f12eea52ae48f91b73a77a4015ec72f8450154
hansoo <zhdnrk124@naver.com>
2024-02-15 22:42:27 +0900

    [25일차] 그리디 - 34번 해설

--- src/main/java/com/algorism/greedy/Greedy_MyAnswer.java
+++ src/main/java/com/algorism/greedy/Greedy_MyAnswer.java
@@ -1,8 +1,10 @@
 package com.algorism.greedy;
 
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Greedy_MyAnswer {
@@ -87,6 +89,55 @@ public class Greedy_MyAnswer {
     }
 
     public void sortCard() {
+
+    }
+    public void bindNumber() {
+        Scanner sc = new Scanner(System.in);
+        //카드 묶음의 수 저장하기
+        int N = sc.nextInt();
+        // 양수는 내림차순 정렬하기
+        PriorityQueue<Integer> plusPq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minusPq = new PriorityQueue<>();
+        int one = 0;
+        int zero = 0;
+
+        //4개의 그룹으로 분리해 저장하기
+        for (int i = 0; i < N; i++) {
+            int data = sc.nextInt();
+            if (data > 1){
+                plusPq.add(data);
+            } else if (data == 1) {
+                one++;
+            } else if (data == 0) {
+                zero++;
+            } else {
+                minusPq.add(data);
+            }
+        }
+        int sum = 0;
+        //양수 처리하기
+        while (plusPq.size() > 1) {
+            int first = plusPq.remove();
+            int second = plusPq.remove();
+            sum = sum + first * second;
+        }
+        if (!plusPq.isEmpty()) {
+            sum = sum + plusPq.remove();
+        }
+        //음수 처리하기
+        while (minusPq.size() > 1) {
+            int first = minusPq.remove();
+            int second = minusPq.remove();
+            sum = sum + first * second;
+        }
+        if (!minusPq.isEmpty()) {
+            if (zero == 0) {
+                sum = sum + minusPq.remove();
+            }
+        }
+        //1 처리하기
+        sum = sum + one;
+        System.out.println(sum);
 
     }
 }
