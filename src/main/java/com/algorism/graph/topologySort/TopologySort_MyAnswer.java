30f45b636aaaa8b7264b2f3bac868be5070aa9ee
hansoo <zhdnrk124@naver.com>
2024-03-05 20:49:08 +0900

    [36일차] 유니온파인드 정리 및 해설

--- src/main/java/com/algorism/graph/topologySort/TopologySort_MyAnswer.java
+++ src/main/java/com/algorism/graph/topologySort/TopologySort_MyAnswer.java
@@ -2,11 +2,46 @@ package com.algorism.graph.topologySort;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class TopologySort_MyAnswer {
 
     public void lineUp() throws IOException {
-
+        Scanner sc = new Scanner(System.in);
+        //노드
+        int N = sc.nextInt();
+        //에지
+        int M = sc.nextInt();
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            A.add(new ArrayList<>());
+        }
+        //진입 차수 배열
+        int[] indegree = new int[N+1];
+        for (int i = 0; i < M; i++) {
+            int S = sc.nextInt();
+            int E = sc.nextInt();
+            A.get(S).add(E);
+            indegree[E]++;
+        }
+        //위상 정렬 수행하기
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            System.out.println(now + " ");
+            for (int next : A.get(now)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
     }
 }
